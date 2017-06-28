cb409614c0d207949031613bce5ba1b76fe1522b
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-06-28 22:51:17 +0200

    Middleware 4.0

--- tests/RequestIdMiddlewareTest.php
+++ tests/RequestIdMiddlewareTest.php
@@ -2,7 +2,6 @@
 
 namespace ApiClients\Tests\Middleware\RequestId;
 
-use ApiClients\Foundation\Middleware\Priority;
 use ApiClients\Middleware\RequestId\Options;
 use ApiClients\Middleware\RequestId\RequestIdMiddleware;
 use ApiClients\Middleware\RequestId\RequestIdStrategy\Uuid4Strategy;
@@ -14,22 +13,12 @@ use function Clue\React\Block\await;
 
 final class RequestIdMiddlewareTest extends TestCase
 {
-    /**
-     * Ensure we run second last so the last can be logging or so that stores information.
-     */
-    public function testPriority()
-    {
-        self::assertSame(
-            Priority::DEFAULT,
-            (new RequestIdMiddleware())->priority()
-        );
-    }
-
     public function testPre()
     {
         $request = await(
             (new RequestIdMiddleware())->pre(
                 new Request('GET', 'https://example.com/'),
+                'abc',
                 [
                     RequestIdMiddleware::class => [
                         Options::STRATEGY => Uuid4Strategy::class,
